93bc8d76f192eb661001e22b59575843241717d5
Vardaan <vardaan209@gmail.com>
2024-03-07 16:21:23 +0530

    test

---
+++ 1Intro.js
@@ -0,0 +1,58 @@
+// console.log("This was a log")
+// const name = "John";
+// console.log(name.toUpperCase());
+// //tHis si a comment lol
+// const rollNum = [1,2,3,4,5];
+// console.log(rollNum);
+// rollNum.push(6);
+// console.log(rollNum);
+// console.log(rollNum.pop());
+// console.log(rollNum.pop());
+// console.log(rollNum.pop());
+// console.log(rollNum);
+
+// const Obj = {
+//     name: "John",
+//     age: 23,
+//     rollNum: 1,
+//     texterhe:{
+//         surname: "Doe",
+//         killer: "John"
+//     }
+// }
+// console.log(Object.entries(Obj));
+// console.log(`My name is ${Obj.name} and my age is ${Obj.age} and my roll number is ${Obj.rollNum} and my surname is ${Obj.texterhe.surname} and my killer is ${Obj.texterhe.killer}`);
+
+
+// function greet(){
+//     console.log("Hello World")
+// }
+// greet()
+
+// const students = [];
+// function addStudent(stuname, stuage, studates, stuaddress){
+
+//     const student = {
+//         name: stuname,
+//         age: stuage,
+//         dates: studates,
+//         address: stuaddress
+//     }
+
+//     students.push(student);
+// }
+
+// addStudent("John", 23, [1,2,3,4], "New York");
+// addStudent("Doe", 24, [5,6,7,8], "Meerut");
+
+// console.log(JSON.stringify(students));
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(5,3));
+
+x = 9;
+;
+console.log(++x);
---
+++ JSpractice2.js
@@ -0,0 +1,21 @@
+console.log("The second JS file");
+var a = 5;
+var b = 6;
+var c;
+c = a+b;
+console.log(c); 
+console.log("Namaste," + c + " Duniya");
+if(c<10)
+{
+    console.log("less than 10");
+}else{
+    console.log("greater than 10, lol");
+}
+
+if ("1"==1 && "1"===1)
+{
+    console.log("using and");
+}
+else if ("1"==1 || "1"===1){
+    console.log("using or");
+}
